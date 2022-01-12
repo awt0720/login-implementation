82ebed0f76c4d1618c055cc1082d5f5b75aa9fbf
wontae <awt0523@gmail.com>
2022-01-12 22:52:47 +0900

    메인 페이지 생성

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,55 +1,51 @@
-import type {NextPage} from "next";
 import {useQuery} from "react-query";
-import {fakeDBApiUrl} from "@common/url";
-import {Row, Col} from "antd";
+import {fakeDBApiUrl} from "@/common/url";
+import {Col, Row} from "antd";
+import {StarFilled} from "@ant-design/icons";
+import Link from "next/link";
+import LoadingPage from "@/components/layout/loadingPage";
+import {IFakeDb} from "@/types/fakeDb";
 
-interface IFakeDb {
-  category: string;
-  description: string;
-  id: number;
-  image: string;
-  price: number;
-  rating: {
-    count: number;
-    rate: number;
-  };
-  title: string;
-}
-
-const Home = () => {
-  const {isLoading, error, data, isFetching} = useQuery<IFakeDb[]>(
+const MainPage = () => {
+  const {isLoading, error, data} = useQuery<IFakeDb[]>(
     "repoData",
     () => fetch(fakeDBApiUrl).then((res) => res.json()),
     {refetchOnWindowFocus: false}
   );
 
-  if (isLoading) return "Loading...";
+  if (isLoading) return <LoadingPage />;
 
   if (error) return "An error has occurred: " + error;
 
-  const style = {background: "#0092ff", padding: "8px 0"};
   return (
-    <>
-      <h2 style={{textAlign: "center", marginBottom: "20px"}}> 판매 목록 </h2>
+    <div className="main-page-wrapper">
+      <h2> 판매 목록 </h2>
       <Row gutter={[16, 24]}>
         {!isLoading &&
           data.map((val) => (
-            <Col className="gutter-row" span={8} key={val.id}>
-              <div className="item-wrapper">
-                <img src={val.image} alt="img" width="100px" height="150px" />
-                <div className="tit">{val.title}</div>
-                <div className="rating">
-                  <div className="count">
-                    누적 판매 개수 {val.rating.count}개
+            <Link href={`/detail/${val.id}`} key={val.id}>
+              <Col className="gutter-row" span={8}>
+                <div className="item-wrapper">
+                  <img src={val.image} alt="img" width="100px" height="150px" />
+                  <div className="title-wrapper">
+                    <div className="title">{val.title}</div>
                   </div>
-                  <div className="rate">평점 {val.rating.rate}</div>
+                  <div className="rating">
+                    <div className="count">
+                      누적 판매 개수 {val.rating.count}개
+                    </div>
+                    <div className="rate">
+                      평점 {val.rating.rate}
+                      <StarFilled />
+                    </div>
+                  </div>
                 </div>
-              </div>
-            </Col>
+              </Col>
+            </Link>
           ))}
       </Row>
-    </>
+    </div>
   );
 };
 
-export default Home;
+export default MainPage;
