e7330fe0d994484c3bd5c2f412a5c98e63d1972c
wontae <awt0523@gmail.com>
2022-01-17 21:59:25 +0900

    회원가입 전화번호 입력 추가

--- components/register.tsx
+++ components/register.tsx
@@ -17,7 +17,14 @@ const Register: React.FC<IRegisterProps> = ({open, setOpen}) => {
     <ModalContainer form={form} open={open} setOpen={setOpen} onCreate={onCreate}>
       <Form form={form} className="form" name="basic" autoComplete="off">
         <Form.Item style={{marginTop: "20px"}} name="name" rules={[{required: true, message: "이름를 입력해주세요."}]}>
-          <Input placeholder="이메일" size="large" className="email-input" />
+          <Input placeholder="이름" size="large" className="email-input" />
+        </Form.Item>
+        <Form.Item
+          style={{marginTop: "20px"}}
+          name="phone"
+          rules={[{required: true, message: "핸드폰 번호를 입력해주세요."}]}
+        >
+          <Input placeholder="핸드폰번호" size="large" className="email-input" />
         </Form.Item>
         <Form.Item name="email" rules={[{required: true, message: "아아디를 입력해주세요."}]}>
           <Input placeholder="아이디 (이메일)" size="large" className="id-input" />
