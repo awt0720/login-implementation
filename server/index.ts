f872df03199c3688ab1aac13b296e76c3278cd67
ahwontae <wontae.ahn@purpleworks.co.kr>
2022-03-01 12:19:51 +0900

    proxy 연습을 위하여 server, client 분리

--- server/index.ts
+++ server/index.ts
@@ -1,11 +1,13 @@
-import express, {urlencoded, Router} from "express";
+import express, { urlencoded } from "express";
 import router from "./routes";
+import dbConnect from "./database";
+
 const app = express();
 const port = process.env.PORT || 4000;
 
 app.use(express.json());
-app.use(urlencoded({extended: true}));
-
+app.use(urlencoded({ extended: true }));
+dbConnect();
 app.use("/api", router);
 
 app.listen(port, () => console.log(`listening at http://localhost:${port}`));
