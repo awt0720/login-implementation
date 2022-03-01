7c456415ca81d12da5675ca21069854552850517
ahwontae <wontae.ahn@purpleworks.co.kr>
2022-03-01 19:10:25 +0900

    로그인, 회원가입 성공시 accessToken 발급

--- server/controller/userController.tsx
+++ server/controller/userController.tsx
@@ -1,6 +1,12 @@
 import { Request, Response } from "express";
+import bcrypt from "bcrypt";
+import dotenv from "dotenv";
+import jwt from "jsonwebtoken";
 import User from "../model/user";
 import { IUser } from "../interface/user";
+dotenv.config();
+
+const secretKey = process.env.SECRET_KEY as string;
 
 const signup = async (req: Request, res: Response) => {
   const data: IUser = req.body;
@@ -22,11 +28,54 @@ const signup = async (req: Request, res: Response) => {
   try {
     const newUser = new User(data);
     const user = await newUser.save();
-    res.send({ success: true, info: user });
+    jwt.sign(
+      { name, email },
+      secretKey,
+      {
+        expiresIn: "1h",
+      },
+      (err, token) => {
+        if (err) throw err;
+        res
+          .status(200)
+          .cookie("accessToken", token, { maxAge: 3600, httpOnly: true })
+          .send({ success: true, user: { name, email }, isLoging: true });
+      }
+    );
   } catch (e) {
     console.log(e);
     res.status(400).send({ success: false, err: e });
   }
 };
 
-export { signup };
+const login = async (req: Request, res: Response) => {
+  console.log("start");
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+
+  if (user) {
+    const validPassword = await bcrypt.compare(password, user.password);
+    if (validPassword) {
+      jwt.sign(
+        { name: user.name, email },
+        secretKey,
+        {
+          expiresIn: "1h",
+        },
+        (err, token) => {
+          if (err) throw err;
+          res
+            .status(200)
+            .cookie("accessToken", token, { maxAge: 3600, httpOnly: true })
+            .send({ success: true, user: { name: user.name, email: user.email }, isLoging: true });
+        }
+      );
+    } else {
+      res.send("비밀번호 불일치");
+    }
+  } else {
+    res.send("유저 없음");
+  }
+};
+
+export { signup, login };
