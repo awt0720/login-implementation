a05035bb14e4a0b1d54af2c54ed5f6ce96f73f52
ahwontae <wontae.ahn@purpleworks.co.kr>
2022-03-01 18:39:15 +0900

    cors 세팅 및 cookie-parser 설치

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,8 +1,17 @@
 import { Router } from "express";
-import { signup } from "../controller/userController";
+import { signup, login } from "../controller/userController";
+import { authorization } from "../controller/authorize";
 
 const router = Router();
 
+router.post("/login", login);
 router.post("/signup", signup);
+router.get("/logout", (req, res) => {
+  // res.clearCookie()
+});
+
+router.get("/authorization", authorization, (req, res) => {
+  res.send({ data: req.body });
+});
 
 export default router;
