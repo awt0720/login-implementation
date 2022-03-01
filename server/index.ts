5792cc89922539fb1fa1a15a01ff945b5ab963cb
ahwontae <wontae.ahn@purpleworks.co.kr>
2022-03-01 13:05:47 +0900

    server cors 설정

--- server/index.ts
+++ server/index.ts
@@ -1,12 +1,24 @@
 import express, { urlencoded } from "express";
+import cors, { CorsOptions } from "cors";
 import router from "./routes";
 import dbConnect from "./database";
+
+const whiteList = ["http://localhost:3000"];
+const corsOptions: CorsOptions = {
+  origin: (option, callback) => {
+    if (whiteList.indexOf(option as string) !== -1) {
+      return callback(null, true);
+    }
+    return callback(new Error("Not Allowed Origin"));
+  },
+};
 
 const app = express();
 const port = process.env.PORT || 4000;
 
 app.use(express.json());
 app.use(urlencoded({ extended: true }));
+app.use(cors(corsOptions));
 dbConnect();
 app.use("/api", router);
 
